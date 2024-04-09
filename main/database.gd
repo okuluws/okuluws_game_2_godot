bd35f619592d6772f265bb0ca584ce13cd42c68c
okuluws <160525909+okuluws@users.noreply.github.com>
2024-04-09 15:22:33 +0200

    importved(shortened) sse code, added subscribe to events

--- main/database.gd
+++ main/database.gd
@@ -4,6 +4,8 @@ extends Node
 @export var url: String
 @export var http_node: HTTPRequest
 
+var subscriptions = {}
+var sse_client_id = null
 
 func parse_res_body(res):
 	return JSON.parse_string(res[3].get_string_from_utf8())
@@ -15,72 +17,47 @@ func _fetch(fetch_url, http_method = HTTPClient.METHOD_GET, data = {}, headers =
 	http_requester.request(fetch_url, headers, http_method, JSON.stringify(data))
 	var res = await http_requester.request_completed
 	if not res[1] == 200 and should_log_on_non_200_code:
-		print_debug("fetch >>%s<< has response >>%s<<" % [fetch_url, JSON.parse_string(res[3].get_string_from_utf8()) if res[3] else null])
+		print_debug("fetch >>%s<< has response code >>%s<<" % [fetch_url, res[1]])
 	
 	remove_child(http_requester)
-	return JSON.parse_string(res[3].get_string_from_utf8()) if res[3] else null
+	return {
+		"code": res[1],
+		"data": JSON.parse_string(res[3].get_string_from_utf8()) if res[3] else null
+	}
+	
 
 
-func get_record(collection_name: String, record_id: String):
-	return await _fetch("%s/api/collections/%s/records/%s" % [url, collection_name, record_id])
+
+func get_record(collection_name: String, record_id: String, query_params: String = ""):
+	return (await _fetch("%s/api/collections/%s/records/%s%s" % [url, collection_name, record_id, query_params])).data
 
 
 func get_records(collection_name: String, query_params: String = "") -> Array:
-	var res = await _fetch("%s/api/collections/%s/records%s" % [url, collection_name, query_params])
-	return res["items"] if res else null
+	return (await _fetch("%s/api/collections/%s/records%s" % [url, collection_name, query_params])).data.items
 
 
 func authenticate(authcollection_name: String, identity: String, password: String, query_params: String = ""):
-	var body = JSON.stringify({
-		"identity": identity,
-		"password": password,
-	})
-	var headers = ["Content-Type: application/json"]
-	http_node.request("%s/api/collections/%s/auth-with-password%s" % [url, authcollection_name, query_params], headers, HTTPClient.METHOD_POST, body)
+	return (await _fetch("%s/api/collections/%s/auth-with-password%s" % [url, authcollection_name, query_params], HTTPClient.METHOD_POST, { "identity": identity, "password": password })).data
+
+
+func create_record(collection_name: String, data: Dictionary, authtoken: String = "", query_params: String = "") -> Dictionary:
+	return (await _fetch("%s/api/collections/%s/records%s" % [url, collection_name, query_params], HTTPClient.METHOD_POST, data, ["Content-Type: application/json", "Authorization: %s" % authtoken if authtoken else ""])).data
+
+
+func update_record(collection_name: String, record_id: String, data: Dictionary, authtoken: String = "", query_params: String = "") -> Dictionary:
+	return (await _fetch("%s/api/collections/%s/records/%s%s" % [url, collection_name, record_id, query_params], HTTPClient.METHOD_PATCH, data, ["Content-Type: application/json", "Authorization: %s" % authtoken if authtoken else ""])).data
+
+
+func subscribe(collection_name: String, action: String, callback: Callable):
+	if not subscriptions.has(collection_name):
+		subscriptions[collection_name] = []
+	subscriptions[collection_name].append({ "action": action, "callback": callback })
 	
-	var res = await http_node.request_completed
-	if res[1] == 200:
-		return parse_res_body(res)
-	else:
-		print_debug(parse_res_body(res))
-		return {}
-
-
-func create_record(collection_name: String, data: Dictionary, authtoken: String = "") -> Dictionary:
-	var body = JSON.stringify(data)
-	var headers = ["Content-Type: application/json"]
-	if authtoken:
-		headers.append("Authorization: %s" % authtoken)
-	
-	http_node.request(url + "/api/collections/%s/records" % collection_name, headers, HTTPClient.METHOD_POST, body)
-	var res = await http_node.request_completed
-	if res[1] == 200:
-		return parse_res_body(res)
-	else:
-		print_debug(parse_res_body(res))
-		return {}
-	
-
-
-func update_record(collection_name: String, record_id: String, data: Dictionary, authtoken: String = "") -> Dictionary:
-	var body = JSON.stringify(data)
-	var headers = ["Content-Type: application/json"]
-	if authtoken:
-		headers.append("Authorization: %s" % authtoken)
-	
-	http_node.request(url + "/api/collections/%s/records/%s" % [collection_name, record_id], headers, HTTPClient.METHOD_PATCH, body)
-	var res = await http_node.request_completed
-	if res[1] == 200:
-		return parse_res_body(res)
-	else:
-		print_debug(parse_res_body(res))
-		return {}
-
-
+	await _fetch("%s/api/realtime" % url, HTTPClient.METHOD_POST, { "clientId": sse_client_id, "subscriptions": subscriptions.keys() }, ["Content-Type: application/json"], false)
 
 # TRYING TO DO SSE, DEFINETLY NOT COPIED CODE
 # EDIT: IT WORKS OMFG HOW?
-func _setup_tcp_stream() -> StreamPeerTCP:
+func _setup_sse_tcp() -> StreamPeerTCP:
 	var tcp = StreamPeerTCP.new()
 	
 	var err_conn_tcp = tcp.connect_to_host("127.0.0.1", 8090)
@@ -108,7 +85,8 @@ func _start_sse_stream(stream: StreamPeer) -> void:
 	request += "\n" # empty line 
 	stream.put_data(request.to_ascii_buffer())
 
-func _read_stream_response(stream: StreamPeer) -> String:
+
+func _read_stream(stream: StreamPeer) -> String:
 	stream.poll()
 	var available_bytes = stream.get_available_bytes()
 	while available_bytes == 0:
@@ -118,63 +96,25 @@ func _read_stream_response(stream: StreamPeer) -> String:
 	
 	return stream.get_string(available_bytes)
 
-class EventData:
-	var type: String
-	var data: Dictionary
 
-func _parse_event_data(event_str: String) -> EventData:
-	var event_lines = event_str.split("\n")
-	if event_lines.size() != 4:
-		return null
-	
-	const EVENT_TYPE_PREFIX = "event:"
-	const EVENT_DATA_PREFIX = "data:"
-	
-	var event_type_line = event_lines[2]
-	if !event_type_line.begins_with(EVENT_TYPE_PREFIX):
-		return null
-	var event_data_line = event_lines[3]
-	if !event_data_line.begins_with(EVENT_DATA_PREFIX):
-		return null
-	
-	var event_type_str = event_type_line.substr(EVENT_TYPE_PREFIX.length())
-	var event_data_str = event_data_line.substr(EVENT_DATA_PREFIX.length())
-	var event_data_json = JSON.parse_string(event_data_str)
-	if event_data_json == null:
-		event_data_json = {}
-	var event = EventData.new()
-	event.type = event_type_str
-	event.data = event_data_json
-	return event
-
-func _parse_response_event_data(response: String) -> Array[Dictionary]:
-	var response_parts = response.replace("\r", "").split("\n\n")
-	var event_data: Array[Dictionary] = []
-	for response_part in response_parts:
-		var event = _parse_event_data(response_part)
-		if event == null:
-			continue
-		
-		event_data.append(event.data)
-	
-	return event_data
-
+# ;)
+func _get_sse_stream_data(stream: StreamPeer):
+	return JSON.parse_string(Array((await _read_stream(stream)).replace("\r", "").split("\n")).filter(func(s): return s.begins_with("data:"))[0].trim_prefix("data:"))
 
 
 func _ready():
-	var tcp = await _setup_tcp_stream()
+	var tcp = await _setup_sse_tcp()
+	_start_sse_stream(tcp)
 	#var stream = await _setup_tls_stream(tcp)
 	
-	_start_sse_stream(tcp)
-	var initial_response = await _read_stream_response(tcp)
-	var client_id = _parse_response_event_data(initial_response)[0]["clientId"]
-	
-	var res = await _fetch("%s/api/realtime" % url, HTTPClient.METHOD_POST, { "clientId": client_id, "subscriptions": ["player_profiles"] }, ["Content-Type: application/json"], false)
-	print(res)
-	
+	sse_client_id = (await _get_sse_stream_data(tcp))["clientId"]
+	await subscribe("player_profiles", "*", func(r): print(r))
+	#await _fetch("%s/api/realtime" % url, HTTPClient.METHOD_POST, { "clientId": sse_client_id, "subscriptions": ["player_profiles"] }, ["Content-Type: application/json"], false)
 	while true:
-		var response = await _read_stream_response(tcp)
-		var events = _parse_response_event_data(response)
-		for event in events:
-			print(event)
+		var event = await _get_sse_stream_data(tcp)
+		if subscriptions.has(event["record"]["collectionName"]):
+			for s in subscriptions[event["record"]["collectionName"]]:
+				if s["action"] == event["action"] or s["action"] == "*":
+					s["callback"].call(event["record"])
+		
 
