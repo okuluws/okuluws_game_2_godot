8d6a34bcc47e367f54c4e6a57ce0a1c30e1c147c
okuluws <160525909+okuluws@users.noreply.github.com>
2024-04-15 15:12:07 +0200

    Update player.gd

--- player/player.gd
+++ player/player.gd
@@ -153,13 +153,14 @@ func set_player_facing_direction(_facing_direction):
 
 func pickup_item(item_data):
 	assert(multiplayer.is_server())
+	var pickup_inventories = { "hotbar": [], "inventory": [] }
+	pickup_inventories["hotbar"].resize(8)
+	pickup_inventories["hotbar"] = pickup_inventories["hotbar"].map(func(_n): return { "capacity": InventorySlotSizes.data["hotbar"], "occupying_item_name": null })
+	pickup_inventories["inventory"].resize(32)
+	pickup_inventories["inventory"] = pickup_inventories["inventory"].map(func(_n): return { "capacity": InventorySlotSizes.data["inventory"], "occupying_item_name": null })
+	# why the fuck does Array.fill() pass by reference, using Array.map() as a workaround 
+	
 	await Server.update_profile_entry(Server.players[user_record_id]["profile_record_id"], "items", func(items):
-		var pickup_inventories = { "hotbar": [], "inventory": [] }
-		pickup_inventories["hotbar"].resize(8)
-		pickup_inventories["hotbar"] = pickup_inventories["hotbar"].map(func(_n): return { "capacity": InventorySlotSizes.data["hotbar"], "occupying_item_name": null })
-		pickup_inventories["inventory"].resize(32)
-		pickup_inventories["inventory"] = pickup_inventories["inventory"].map(func(_n): return { "capacity": InventorySlotSizes.data["inventory"], "occupying_item_name": null })
-		# why the fuck does Array.fill() pass by reference, using Array.map() as a workaround 
 		
 		items.map(func(item):
 			if item.inventory_name in pickup_inventories:
