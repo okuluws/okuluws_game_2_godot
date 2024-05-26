005753f08bd84463d4c83d0dd4f3aac733d37415
okuluws <160525909+okuluws@users.noreply.github.com>
2024-05-26 09:20:55 +0200

    q

--- home/game_options.gd
+++ home/game_options.gd
@@ -5,48 +5,46 @@ extends CanvasLayer
 @onready var virtual_joystick_value_label = $"TabContainer/General/VirtualJoystickButton/Label2"
 @onready var content_scale_size_value_label = $"TabContainer/General/ResolutionButton/Label2"
 @onready var save_button = $"Save"
-var config = ConfigFile.new()
-var start_config_hash
 
 
 func _ready():
-	if config.load("user://options.cfg") != OK: push_error(); _on_back_pressed(); return
-	start_config_hash = config.encode_to_text().hash()
 	_display_config()
 
 
 func _display_config():
-	virtual_joystick_value_label.text = "Enabled" if config.get_value("gameplay", "virtual_joystick") else "Disabled"
-	var content_scale_size_value = config.get_value("video", "content_scale_size")
+	virtual_joystick_value_label.text = "Enabled" if home.config.get_value("gameplay", "virtual_joystick") else "Disabled"
+	var content_scale_size_value = home.config.get_value("video", "content_scale_size")
 	content_scale_size_value_label.text = "%dx%d" % [content_scale_size_value.x, content_scale_size_value.y]
 	
-	save_button.disabled = config.encode_to_text().hash() == start_config_hash
+	save_button.disabled = home.config.encode_to_text().hash() == home.latest_load_config_hash
 	
 	
 
 
 func _on_back_pressed():
+	home.config.clear()
+	home.load_config()
 	home.add_child(load("res://home/title_screen.tscn").instantiate())
 	queue_free()
 
 
 func _on_virtual_joystick_button_pressed():
-	config.set_value("gameplay", "virtual_joystick", not config.get_value("gameplay", "virtual_joystick"))
+	home.config.set_value("gameplay", "virtual_joystick", not home.config.get_value("gameplay", "virtual_joystick"))
 	_display_config()
 
 
 func _on_resolution_button_pressed():
-	config.set_value("video", "content_scale_size", Vector2(1920, 1080))
+	home.config.set_value("video", "content_scale_size", Vector2(1920, 1080))
 	_display_config()
 
 
 func _on_save_pressed():
-	config.save("user://options.cfg")
+	home.save_config()
 	home.load_config()
 	_ready()
 
 
 func _on_reset_config_button_pressed():
-	home.setup_standart_config()
+	home.save_standart_config()
 	home.load_config()
 	_ready()
--- home/main.gd
+++ home/main.gd
@@ -4,24 +4,30 @@ extends CanvasLayer
 const CONFIG_PATH = "user://options.cfg"
 @onready var pocketbase = $"../Pocketbase"
 var config = ConfigFile.new()
+var latest_load_config_hash
 
 
 func _ready():
 	if not FileAccess.file_exists(CONFIG_PATH):
-		setup_standart_config()
+		save_standart_config()
 	load_config()
 	pocketbase.ready.connect(func(): add_child(load("res://home/title_screen.tscn").instantiate()))
 	
 
 
-func setup_standart_config():
+func save_standart_config():
 	config.clear()
 	config.set_value("gameplay", "virtual_joystick", OS.has_feature("mobile"))
 	config.set_value("video", "content_scale_size", Vector2(1152, 648))
-	if config.save(CONFIG_PATH) != OK: push_error(); return
-	
+	save_config()
 
 
 func load_config():
 	if config.load(CONFIG_PATH) != OK: push_error(); return
+	latest_load_config_hash = config.encode_to_text().hash()
 	get_tree().root.content_scale_size = config.get_value("video", "content_scale_size")
+
+
+func save_config():
+	if config.save(CONFIG_PATH) != OK: push_error(); return
+	
