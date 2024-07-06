f8884c5d0a0d389b39a05b634a5239004b0cfda4
okuluws <160525909+okuluws@users.noreply.github.com>
2024-07-06 16:57:38 +0200

    yoo3425923549825

--- main/home/game_settings_screen.gd
+++ main/home/game_settings_screen.gd
@@ -6,14 +6,23 @@ var home
 @export var current_user_label: Label
 @export var login_username_edit: LineEdit
 @export var login_password_edit: LineEdit
+@export var login_request_status_label: RichTextLabel
 @export var register_username_edit: LineEdit
 @export var register_password_edit: LineEdit
+@export var register_request_status_label: RichTextLabel
 @onready var main = home.main
 @onready var pocketbase = main.modules.pocketbase
 
 
 func _ready():
-	pass
+	_update_auth_info_label()
+
+
+func _update_auth_info_label():
+	if pocketbase.has_current_auth():
+		current_user_label.text = "Current user: %s" % pocketbase.get_current_username()
+	else:
+		current_user_label.text = "Not logged in."
 	
 
 
@@ -23,12 +32,27 @@ func _on_back_pressed():
 
 
 func _on_logout_button_pressed():
-	pass
+	if not pocketbase.has_current_auth(): return
+	pocketbase.unset_current_auth()
+	_update_auth_info_label()
 
 
 func _on_login_button_pressed():
-	pass # Replace with function body.
+	pocketbase.auth_with_password("users", login_username_edit.text, login_password_edit.text, func(res):
+		if res.err != null:
+			push_error("couldn't login")
+			login_request_status_label.text = "[color=red]something went wrong"
+		else:
+			login_request_status_label.text = "[color=green]success"
+			_update_auth_info_label()
+	)
 
 
 func _on_register_button_pressed():
-	pass # Replace with function body.
+	pocketbase.create_record("users", { "username": register_username_edit.text, "password": register_password_edit.text, "passwordConfirm": register_password_edit.text }, func(res):
+		if res.err != null:
+			push_error("couldn't register")
+			register_request_status_label.text = "[color=red]something went wrong"
+		else:
+			register_request_status_label.text = "[color=green]success"
+	)
--- main/pocketbase/pocketbase.gd
+++ main/pocketbase/pocketbase.gd
@@ -3,7 +3,7 @@ extends Node
 
 signal current_auth_updated
 
-var address = "127.0.0.1"
+var address = "192.168.178.214"
 var port = 20071
 
 var auth_save_path: String = "user://pocketbase_auth.cfg"
@@ -19,20 +19,28 @@ var _auth_cfg = ConfigFile.new()
 
 
 func start() -> void:
+	var callback_lock = false
 	if FileAccess.file_exists(auth_save_path):
 		_auth_cfg_load()
 	else:
 		_auth_cfg_save()
 	
-	
 	var t_start = Time.get_ticks_msec()
+	callback_lock = true
 	api_health_check(func(res):
 		if res.err != null: push_error("pocketbase server doesnt want me ;(")
-		else: print("api response time: %dms" % Time.get_ticks_msec() - t_start)
+		else: print("pocketbase api response time: %dms" % (Time.get_ticks_msec() - t_start))
+		callback_lock = false
 	)
+	while callback_lock: await get_tree().process_frame
 	
 	if has_current_auth():
-		refresh_current_auth(func(res): if res.err != null or res.code != 200: push_error("couldn't refresh auth"))
+		callback_lock = true
+		refresh_current_auth(func(res):
+			if res.err != null or res.code != 200: push_error("couldn't refresh auth")
+			callback_lock = false
+		)
+		while callback_lock: await get_tree().process_frame
 	
 	_start_realtime()
 
@@ -43,77 +51,90 @@ func api_health_check(callback: Callable) -> void:
 				callback.call({ "err": "idk" })
 			else:
 				callback.call({ "err": null }),
-		)
+		"", HTTPClient.METHOD_GET, ["content-type:application/json"])
 
 
 func create_record(collection_id: String, data: Dictionary, callback: Callable, use_auth_header: bool = false) -> void:
-	_request(_get_collection_url(collection_id) + "/records", func(err, code, body, _headers):
-			if err != null or code != 200:
+	_request(_get_collection_url(collection_id) + "/records", func(res):
+			if res.err != null or res.code != 200:
 				callback.call({ "err": "idk" })
 			else:
-				callback.call({ "err": null, "record": JSON.parse_string(body.get_string_from_utf8()) }),
-		JSON.stringify(data), HTTPClient.METHOD_POST, ["Authorization: %s" % _get_current_authtoken()] if use_auth_header else [])
+				callback.call({ "err": null, "record": JSON.parse_string(res.body.get_string_from_utf8()) }),
+		JSON.stringify(data), HTTPClient.METHOD_POST, ["content-type:application/json"] + ["Authorization: %s" % _get_current_authtoken()] if use_auth_header else [])
 
 
 func delete_record(collection_id: String, record_id: String, callback: Callable, use_auth_header: bool = false) -> void:
-	_request(_get_record_url(collection_id, record_id), func(err, code, _body, _headers):
-			if err != null or code != 204:
+	_request(_get_record_url(collection_id, record_id), func(res):
+			if res.err != null or res.code != 204:
 				callback.call({ "err": "idk" })
 			else:
 				callback.call({ "err": null }),
-		"", HTTPClient.METHOD_DELETE, ["Authorization: %s" % _get_current_authtoken()] if use_auth_header else [])
+		"", HTTPClient.METHOD_DELETE, ["content-type:application/json"] + ["Authorization: %s" % _get_current_authtoken()] if use_auth_header else [])
 
 
 func update_record(collection_id: String, record_id: String, data: Dictionary, callback: Callable, use_auth_header: bool = false) -> void:
-	_request(_get_record_url(collection_id, record_id), func(err, code, body, _headers):
-			if err != null or code != 200:
+	_request(_get_record_url(collection_id, record_id), func(res):
+			if res.err != null or res.code != 200:
 				callback.call({ "err": "idk" })
 			else:
-				callback.call({ "err": null, "record": JSON.parse_string(body.get_string_from_utf8()) }),
-		JSON.stringify(data), HTTPClient.METHOD_PATCH, ["Authorization: %s" % _get_current_authtoken()] if use_auth_header else [])
+				callback.call({ "err": null, "record": JSON.parse_string(res.body.get_string_from_utf8()) }),
+		JSON.stringify(data), HTTPClient.METHOD_PATCH, ["content-type:application/json"] + ["Authorization: %s" % _get_current_authtoken()] if use_auth_header else [])
 
 
 func get_single_record(collection_id: String, record_id: String, callback: Callable, use_auth_header: bool = false) -> void:
-	_request(_get_record_url(collection_id, record_id), func(err, code, body, _headers):
-			if err != null or code != 200:
+	_request(_get_record_url(collection_id, record_id), func(res):
+			if res.err != null or res.code != 200:
 				callback.call({ "err": "idk" })
 			else:
-				callback.call({ "err": null, "record": JSON.parse_string(body.get_string_from_utf8()) }),
-		"", HTTPClient.METHOD_GET, ["Authorization: %s" % _get_current_authtoken()] if use_auth_header else [])
+				callback.call({ "err": null, "record": JSON.parse_string(res.body.get_string_from_utf8()) }),
+		"", HTTPClient.METHOD_GET, ["content-type:application/json"] + ["Authorization: %s" % _get_current_authtoken()] if use_auth_header else [])
 
 
 func auth_with_password(auth_collection_id: String, identity: String, password: String, callback: Callable) -> void:
-	_request(_get_collection_url(auth_collection_id) + "/auth-with-password", func(err, code, body, _headers):
-			if err != null or code != 200:
+	_request(_get_collection_url(auth_collection_id) + "/auth-with-password", func(res):
+			if res.err != null or res.code != 200:
 				callback.call({ "err": "idk" })
 			else:
-				var parsed_body = JSON.parse_string(body.get_string_from_utf8())
-				_auth_cfg.set_value(var_to_str({ "collection_id": parsed_body.record.collectionId, "record_id": parsed_body.record.id }), "authtoken", parsed_body.token)
+				var parsed_body = JSON.parse_string(res.body.get_string_from_utf8())
+				var section = var_to_str({ "collection_id": parsed_body.record.collectionId, "record_id": parsed_body.record.id })
+				_auth_cfg.set_value(section, "authtoken", parsed_body.token)
+				_auth_cfg.set_value(section, "username", parsed_body.record.username)
 				current_auth_collection_id = parsed_body.record.collectionId
 				current_auth_record_id = parsed_body.record.id
+				_auth_cfg_save()
 				current_auth_updated.emit()
-				callback.call({ "err": null, "res": parsed_body }),
-		{ "identity": identity, "password": password }, HTTPClient.METHOD_POST)
-
-
-func auth_register(auth_collection_id: String, username: String, password: String, callback: Callable) -> void:
-	create_record(auth_collection_id, { "username": username, "password": password, "passwordConfirm": password }, callback)
+				callback.call({ "err": null, "record": parsed_body.record }),
+		JSON.stringify({ "identity": identity, "password": password }), HTTPClient.METHOD_POST, ["content-type:application/json"])
 
 
 func refresh_current_auth(callback: Callable) -> void:
-	_request(_get_collection_url(current_auth_collection_id) + "/auth-refresh", func(err, code, body, _headers):
-			if err != null or code != 200:
+	_request(_get_collection_url(current_auth_collection_id) + "/auth-refresh", func(res):
+			if res.err != null or res.code != 200:
 				callback.call({ "err": "idk" })
 			else:
-				var parsed_body = JSON.parse_string(body.get_string_from_utf8())
-				_auth_cfg.set_value(var_to_str({ "collection_id": parsed_body.record.collectionId, "record_id": parsed_body.record.id }), "authtoken", parsed_body.token)
+				var parsed_body = JSON.parse_string(res.body.get_string_from_utf8())
+				var section = var_to_str({ "collection_id": parsed_body.record.collectionId, "record_id": parsed_body.record.id })
+				_auth_cfg.set_value(section, "authtoken", parsed_body.token)
+				_auth_cfg.set_value(section, "username", parsed_body.record.username)
+				_auth_cfg_save()
 				current_auth_updated.emit()
-				callback.call({ "err": null, "res": parsed_body }),
-	{}, HTTPClient.METHOD_POST, ["Authorization: %s" % _get_current_authtoken()])
+				callback.call({ "err": null, "record": parsed_body.record }),
+	{}, HTTPClient.METHOD_POST, ["content-type:application/json", "Authorization: %s" % _get_current_authtoken()])
 
 
 func has_current_auth() -> bool:
 	return _auth_cfg.has_section_key("", "current_auth_collection_id") and _auth_cfg.has_section_key("", "current_auth_record_id")
+
+
+func unset_current_auth() -> void:
+	current_auth_collection_id = null
+	current_auth_record_id = null
+	_auth_cfg_save()
+	current_auth_updated.emit()
+
+
+func get_current_username():
+	return _auth_cfg.get_value(_get_auth_cfg_section(current_auth_collection_id, current_auth_record_id), "username")
 
 
 #func subscribe(path: String, on_event: Callable, event_action: String = "*") -> void: pass
@@ -138,6 +159,7 @@ func _get_current_authtoken():
 
 func _get_auth_cfg_section(collection_id, record_id):
 	for section in _auth_cfg.get_sections():
+		if section.is_empty(): continue
 		var parsed_section = str_to_var(section)
 		if parsed_section.collection_id == collection_id and parsed_section.record_id == record_id:
 			return section
@@ -151,12 +173,12 @@ func _auth_cfg_load():
 	if _auth_cfg.load(auth_save_path) != OK: push_error("couldn't load auth config")
 
 
-func _request(url, on_request_completed: Callable, request_data = "", method = HTTPClient.METHOD_GET, custom_headers = []):
+func _request(url, on_request_completed, request_data, method, custom_headers):
 	var temp_http_node = HTTPRequest.new()
 	temp_http_node.timeout = 4
 	temp_http_node.request_completed.connect(func(result, response_code, headers, body):
 		if result != HTTPRequest.RESULT_SUCCESS:
-			on_request_completed.call({ "err": "request failed you" })
+			on_request_completed.call({ "err": "couldn't request %s" % url })
 			temp_http_node.queue_free()
 			return
 		on_request_completed.call({ "err": null, "code": response_code, "body": body, "headers": headers })
@@ -167,7 +189,6 @@ func _request(url, on_request_completed: Callable, request_data = "", method = H
 	if temp_http_node.request(url, PackedStringArray(custom_headers), method, request_data) != OK:
 		on_request_completed.call({ "err": "couldn't create request" })
 		temp_http_node.queue_free()
-		return
 
 
 func _start_realtime(): pass
